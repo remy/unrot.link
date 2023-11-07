abc322c341a7a80a7364ee01f1056daf82e083cb
Remy Sharp <remy@remysharp.com>
2023-11-07 21:40:10 +0000

    feat: add ping endpoint

---
+++ netlify/edge-functions/ping/ping.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'https://edge.netlify.com';
+
+export default function () {
+  return new Response(null, {
+    status: 206,
+    headers: {
+      'cache-control': `public, s-maxage=${3600 * 24}`, // 1 day
+    },
+  });
+}
+
+export const config: Config = {
+  path: '/ping',
+  cache: 'manual',
+};
